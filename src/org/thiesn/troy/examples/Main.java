0d54cf34e663381a984f9fd25a459f51e6fd74af
Marcus Thiesen <marcus.thiesen@freiheit.com>
2010-09-01 22:55:23 +0200

    Fix GPL Header

--- src/org/thiesn/troy/examples/Main.java
+++ src/org/thiesn/troy/examples/Main.java
@@ -1,20 +1,20 @@
 /*
  * (c) Copyright 2010 Marcus Thiesen (marcus@thiesen.org)
  *
- *  This file is part of jiffs.
+ *  This file is part of TroyORM.
  *
- *  jiffs is free software: you can redistribute it and/or modify
+ *  TroyORM is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  jiffs is distributed in the hope that it will be useful,
+ *  TroyORM is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with jiffs.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with TroyORM.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
